8ef0b616f36754d0ab533fdf56a9a2d6b9d408bc
Willson Purl <purlwillson786@gmail.com>
2021-03-06 15:00:08 -0500

    Favicon added

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default class CustomDocument extends Document {
             <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
             <meta name="robots" content="follow, index" />
             <link rel="canonical" href="https://bssteelfabricator.com/" />
-            <link rel="shortcut icon" href="/img/logo.svg" type="image/x-icon"/>
+            <link rel="shortcut icon" href="/img/logo.ico" type="image/x-icon"/>
             <script async src="https://kit.fontawesome.com/f5be5aaf66.js" crossOrigin="anonymous"></script>
             </Head>
             <body className="font-body text-primary-800">
